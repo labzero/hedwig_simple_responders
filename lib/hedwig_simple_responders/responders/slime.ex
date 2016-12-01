1dc296666253056e7e8378e9b398e1022e208bcb
Robert Adams <radams@labzero.com>
2016-12-01 07:14:43 -0800

    Add slime

---
+++ lib/hedwig_simple_responders/responders/slime.ex
@@ -0,0 +1,23 @@
+defmodule HedwigSimpleResponders.Slime do
+  @moduledoc """
+  You can't do that on slack
+  """
+  use Hedwig.Responder
+
+  @usage """
+  """
+
+  hear ~r/^(i don['’]?t know)$/i, message do
+    slimes = [
+      "http://media1.giphy.com/media/ASkzwxye3Tqog/giphy.gif",
+      "http://media3.giphy.com/media/cXqN42Xaj3TTW/giphy.gif",
+      "http://media.giphy.com/media/JJEeGvzBX0UjS/giphy.gif",
+      "http://25.media.tumblr.com/tumblr_lzvxu76oZR1qaphrco1_500.gif",
+      "https://cdn2.vox-cdn.com/thumbor/DRuG57jAsWOcZIL5GMQCadrP8mA=/cdn0.vox-cdn.com/uploads/chorus_asset/file/2919370/achildslimed.0.gif",
+      "http://stream1.gifsoup.com/view8/4635444/slime-2-o.gif",
+      "https://38.media.tumblr.com/tumblr_m80cypMFbZ1qm6uzxo1_250.gif",
+      "http://media.giphy.com/media/13oY2AWk5RV5kI/giphy.gif"
+      ]
+    send message, random(slimes)
+  end
+end
