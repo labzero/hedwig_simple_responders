cc23f88c9d902b84dbb27988a65a0367aca0454c
Brien Wankel <brien@reebosak.net>
2016-12-01 19:51:15 -0700

    Refactor trout_slap.ex for readability

--- lib/hedwig_simple_responders/responders/trout_slap.ex
+++ lib/hedwig_simple_responders/responders/trout_slap.ex
@@ -8,15 +8,11 @@ defmodule HedwigSimpleResponders.TroutSlap do
   slap - Slaps the target around a bit with a large trout
   """
 
-  hear ~r/slap me/i, message do
+  hear ~r/^slap me/i, message do
     emote(message, "slaps #{message.user.name} around a bit with a large trout")
   end
-
-  hear ~r/slaps?\s*(.+)/i, message do
-    target = Map.get(message.matches, 1)
-    case target do
-      "me" -> :ok
-      _ -> emote(message, "slaps #{target} around a bit with a large trout")
-    end
+  hear ~r/^slaps me/i, message, do: :ok
+  hear ~r/^slaps?\s*(?<target>\w+).*/i, message do
+    emote(message, "slaps #{message.matches["target"]} around a bit with a large trout")
   end
 end
