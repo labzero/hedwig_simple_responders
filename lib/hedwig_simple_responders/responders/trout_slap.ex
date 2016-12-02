578f28e321f1288c56af233038654fa4fcbf04ad
Brien Wankel <brien@reebosak.net>
2016-12-01 20:13:28 -0700

    Add Slack user id pattern match for trout_slap

--- lib/hedwig_simple_responders/responders/trout_slap.ex
+++ lib/hedwig_simple_responders/responders/trout_slap.ex
@@ -15,4 +15,8 @@ defmodule HedwigSimpleResponders.TroutSlap do
   hear ~r/^slaps?\s*(?<target>\w+).*/i, message do
     emote(message, "slaps #{message.matches["target"]} around a bit with a large trout")
   end
+  # match slack mentions which come over as <@U0DM97L3T>
+  hear ~r/^slaps?\s*<@(?<target>\w+)>.*/i, message do
+    emote(message, "slaps <@#{message.matches["target"]}> around a bit with a large trout")
+  end
 end
