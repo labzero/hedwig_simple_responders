c3a8e574bf1dba27f4f46b2f694554fdd540d595
Sasha Voynow <sasha@labzero.com>
2016-12-07 14:18:59 -0800

    remove github responder (moved to a separate library)

--- lib/hedwig_simple_responders/github/api_client.ex
+++
@@ -1,8 +0,0 @@
-defmodule HedwigSimpleResponders.Github.ApiClient do
-
-  @doc "return the latest n commits from the specified repo"
-  @callback commits(org :: String.t, repo :: String.t, limit :: integer(), access_token :: String.t) :: [any] 
-
-  @doc "shorten the given url"
-  @callback shorten_url(url :: String.t) :: String.t
-end
