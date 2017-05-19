97a808eb2d2a8936b5374d413d921bbe5af4c580
Brien Wankel <brien@reebosak.net>
2017-05-19 10:41:49 -0700

    Fix sloganator encoding issue

--- test/responders/slogan_test.exs
+++ test/responders/slogan_test.exs
@@ -7,4 +7,11 @@ defmodule HedwigSimpleResponders.SloganTest do
     assert_receive {:message, %{text: text}}, 5_000
     assert String.contains?(text, "Much Slogans")
   end
+
+  @tag start_robot: true, name: "alfred", responders: [{HedwigSimpleResponders.Slogan, []}]
+  test "slogan <stuff>", %{adapter: adapter, msg: msg} do
+    send adapter, {:message, %{msg | text: "slogan Slogan's & Stuff"}}
+    assert_receive {:message, %{text: text}}, 5_000
+    assert String.contains?(text, "Slogan's & Stuff")
+  end
 end
